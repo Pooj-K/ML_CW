2b92ca6f607e1761f1915f0ad0b790f657ded8af
Pooj-K <101263160+Pooj-K@users.noreply.github.com>
2023-02-25 12:38:02 +0530

    Creat KNN model 1 and test/train

--- testing.py
+++ testing.py
@@ -16,4 +16,12 @@ y = data.iloc[:, -1]
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+# Create the KNN model
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Train the model on the training set
+knn.fit(X_train, y_train)
+
+# Test the model on the testing set
+y_pred = knn.predict(X_test)
 
