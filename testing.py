6c84c0f824cb83ba459354e18404f67d982da920
Pooj-K <101263160+Pooj-K@users.noreply.github.com>
2023-02-25 12:30:25 +0530

    load data set

--- testing.py
+++ testing.py
@@ -1,1 +1,18 @@
-print("hello world")
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+
+# Load the dataset
+data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data", header=None)
+
+# Split the data into features and labels
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
