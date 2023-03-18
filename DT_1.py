8b81a1c1b7583ca5b1aabee681c3fd183dd9bc34
Pooj-K <101263160+Pooj-K@users.noreply.github.com>
2023-03-18 14:24:38 +0530

    import necessary libraries

---
+++ DT_1.py
@@ -0,0 +1,8 @@
+# Import required libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, plot_tree
