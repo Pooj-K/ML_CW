84f930cb6b6decd30d1e6363a60c9c07a05304a1
Pooj-K <101263160+Pooj-K@users.noreply.github.com>
2023-03-18 13:35:21 +0530

    import libraries, load dataset, set column names

--- testing.py
+++ testing.py
@@ -5,31 +5,32 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import matplotlib.pyplot as plt
+import os, sys, numpy as np
+import matplotlib.pyplot as plt
+import numpy.random as rnd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import LocalOutlierFactor
 
 # Load the dataset
 data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data", header=None)
 
-# Split the data into features and labels
-X = data.iloc[:, :-1]
-y = data.iloc[:, -1]
+# Set column names
+column_names = ["word_freq_make", "word_freq_address", "word_freq_all", "word_freq_3d", "word_freq_our", "word_freq_over",
+                "word_freq_remove", "word_freq_internet", "word_freq_order", "word_freq_mail", "word_freq_receive",
+                "word_freq_will", "word_freq_people", "word_freq_report", "word_freq_addresses", "word_freq_free",
+                "word_freq_business", "word_freq_email", "word_freq_you", "word_freq_credit", "word_freq_your",
+                "word_freq_font", "word_freq_000", "word_freq_money", "word_freq_hp", "word_freq_hpl",
+                "word_freq_george", "word_freq_650", "word_freq_lab", "word_freq_labs", "word_freq_telnet",
+                "word_freq_857", "word_freq_data", "word_freq_415", "word_freq_85", "word_freq_technology",
+                "word_freq_1999", "word_freq_parts", "word_freq_pm", "word_freq_direct", "word_freq_cs",
+                "word_freq_meeting", "word_freq_original", "word_freq_project", "word_freq_re", "word_freq_edu",
+                "word_freq_table", "word_freq_conference", "char_freq_;", "char_freq_(", "char_freq_[", "char_freq_!",
+                "char_freq_$", "char_freq_#", "capital_run_length_average", "capital_run_length_longest",
+                "capital_run_length_total", "is_spam"]
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# Set the column names of the dataframe
+data.columns = column_names
 
-# Create the KNN model
-knn = KNeighborsClassifier(n_neighbors=5)
-
-# Train the model on the training set
-knn.fit(X_train, y_train)
-
-# Test the model on the testing set
-y_pred = knn.predict(X_test)
-
-# Evaluate the model performance
-acc_score = accuracy_score(y_test, y_pred)
-conf_matrix = confusion_matrix(y_test, y_pred)
-class_report = classification_report(y_test, y_pred)
-
-print("Accuracy score: ", acc_score)
-print("Confusion matrix:\n", conf_matrix)
-print("Classification report:\n", class_report)
+print(data)
