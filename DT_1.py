a6d9f34cbb327e4229b5a23deb557900a78b41f8
Pooj-K <101263160+Pooj-K@users.noreply.github.com>
2023-03-19 13:56:08 +0530

    Accuracy vs alpha for training and testing sets

--- DT_1.py
+++ DT_1.py
@@ -159,3 +159,19 @@ ax[1].set_ylabel("depth of tree")
 ax[1].set_title("Depth vs alpha")
 fig.tight_layout()
 plt.show()
+
+train_scores = [clf.score(X_train, y_train) for clf in clfs]
+test_scores = [clf.score(X_test, y_test) for clf in clfs]
+
+fig, ax = plt.subplots(figsize=(10,8))
+ax.set_xlabel("alpha")
+ax.set_ylabel("accuracy")
+ax.set_title("Accuracy vs alpha for training and testing sets")
+ax.plot(ccp_alphas, train_scores, marker='o', label="train",
+        drawstyle="steps-post")
+ax.plot(ccp_alphas, test_scores, marker='o', label="test",
+        drawstyle="steps-post")
+ax.legend()
+plt.grid()
+plt.show()
+
