bfdd10a9ddd703bb3eb0b64b8b8b6b5eade75a5d
Pooj-K <101263160+Pooj-K@users.noreply.github.com>
2023-03-19 13:53:16 +0530

    Number of nodes in the last tree

--- DT_1.py
+++ DT_1.py
@@ -133,4 +133,12 @@ ax.plot(ccp_alphas[:-1], impurities[:-1], marker='o', drawstyle="steps-post")
 ax.set_xlabel("effective alpha")
 ax.set_ylabel("total impurity of leaves")
 ax.set_title("Total Impurity vs effective alpha for training set")
+plt.show()
 
+clfs = []
+for ccp_alpha in ccp_alphas:
+    clf = DecisionTreeClassifier(random_state=0, ccp_alpha=ccp_alpha)
+    clf.fit(X_train, y_train)
+    clfs.append(clf)
+print("Number of nodes in the last tree is: {} with ccp_alpha: {}".format(
+      clfs[-1].tree_.node_count, ccp_alphas[-1]))
